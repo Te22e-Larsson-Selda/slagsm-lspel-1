54d72c229cf4c12248ed28471e803c5f4844b00d
Selda Larsson <selda.larssonsvensdotter@elev.ga.ntig.se>
2023-09-21 10:56:52 +0200

    Start

---
+++ Slagsmål_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// SLUMP
+Random generator = new Random();
+
+int slumptal =generator.Next(10);
+
+Console.WriteLine(slumptal);
+
+
+// WHILE-LOOP
+
+int tal = 100;
+while (tal > 0)
+{
+    Console.WriteLine(tal);
+    tal--;
+}
+
+Console.ReadLine();
