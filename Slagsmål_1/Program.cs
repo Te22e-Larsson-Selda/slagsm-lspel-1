5df744b9f042faeef8a287c92302f5cd984ddbf3
Selda Larsson <selda.larssonsvensdotter@elev.ga.ntig.se>
2023-09-26 11:08:21 +0200

    Inte klar

--- Slagsmål_1/Program.cs
+++ Slagsmål_1/Program.cs
@@ -1,18 +1,18 @@
-﻿// SLUMP
-Random generator = new Random();
+﻿Random generator = new Random();
+int Jhp = 100;
+int Mhp = 75;
+int Runda = 1;
+Console.WriteLine("Hej Hej, välj ditt namn");
+string Jnamn = Console.ReadLine();
 
-int slumptal =generator.Next(10);
-
-Console.WriteLine(slumptal);
-
-
-// WHILE-LOOP
-
-int tal = 100;
-while (tal > 0)
+while (Jhp > 0 & Mhp > 0)
 {
-    Console.WriteLine(tal);
-    tal--;
-}
+    Console.WriteLine(Runda);
+    Runda++;
 
+    int slumptal = generator.Next(10,40);  
+    Mhp = Mhp - slumptal;
+    Console.WriteLine("Motstondaren tog" slumptal " i skadda och har nu " Mhp);
+      
+}
 Console.ReadLine();
